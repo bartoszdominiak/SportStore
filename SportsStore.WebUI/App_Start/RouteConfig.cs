ee3fe445b027542785b4a149ed6a91164ca2ebd3
Bartas199 <bartoszjandominiak@gmail.com>
2018-09-03 20:44:29 +0200

    Add category filter

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -13,17 +13,49 @@ namespace SportsStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Strona{page}",
-                defaults: new { Controller = "Product", action = "List" }
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                });
+
+            routes.MapRoute(null,
+                "Strona(page)",
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" }
                 );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
-            );
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+                );
+
+            routes.MapRoute(null,
+                "{category}/Strona{page}",
+                new
+                {
+                    controller = "Product",
+                    action = "List"
+                });
+
+            routes.MapRoute(null, "{controller}/{action}");
+
+
+            //routes.MapRoute(
+            //    name: null,
+            //    url: "Strona{page}",
+            //    defaults: new { Controller = "Product", action = "List" }
+            //    );
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+            //);
         }
     }
 }
--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -20,17 +20,20 @@ namespace SportsStore.WebUI.Controllers
             this.repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
-                Products = repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+                Products = repository.Products
+                .Where(p => p.Category == category || category == null)
+                .OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
                 PagingInfo = new PagingInfo
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotaItems = repository.Products.Count()
-                }
+                },
+                CurretCategory = category
             };
             return View(model);
         }
